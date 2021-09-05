1dd5f960a9e88f30181c1cb6bb36f6e3a3db8f5f
Jaskievich <yskevich@tut.by>
2021-09-05 22:27:06 +0300

    Signed-off-by: Jaskievich <yskevich@tut.by>

--- app/src/main/java/com/example/Reminder/MyReceiver.java
+++ app/src/main/java/com/example/Reminder/MyReceiver.java
@@ -2,6 +2,9 @@ package com.example.Reminder;
 
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,13 +13,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 
+import androidx.core.app.NotificationCompat;
+
 import java.util.Date;
+import java.util.Objects;
 
 import static android.content.Context.ALARM_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver
 {
-
+    public static final String TAG_NOTIFICATION = "NOTIFICATION_MESSAGE";
+    public static final String CHANNEL_ID = "channel_1111";
+    public static final int NOTIFICATION_ID = 111111;
     @Override
     public void onReceive(Context context, Intent _intent)
     {
@@ -26,16 +34,27 @@ public class MyReceiver extends BroadcastReceiver
         if (bundle == null) return;
         ReminderItem item = (ReminderItem) bundle.getSerializable("item");
         if ( item == null ) return;
-        // Разбудить экран
-        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        @SuppressLint("InvalidWakeLockTag")
-        PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
-        wakeLock.acquire();
-        Intent intent = new Intent(context, ActivityItemView.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra(ReminderItem.class.getSimpleName(), item);
-        context.startActivity(intent);
-        wakeLock.release();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                startNotification(context, NOTIFICATION_ID,item);
+        }
+        else {
+            // Разбудить экран
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            @SuppressLint("InvalidWakeLockTag")
+            PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+            //   PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "TAG");
+            wakeLock.acquire();
+            try {
+                Intent intent = new Intent(context, ActivityItemView.class);
+                //      _intent.setClass(context, ActivityItemView.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.putExtra(ReminderItem.class.getSimpleName(), item);
+                context.startActivity(_intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            wakeLock.release();
+        }
         // Запустить следующее задание
         RemindDBHelper remindDBHelper = new RemindDBHelper(context);
         ReminderItem item_peek = remindDBHelper.getActualFistItem();
@@ -78,6 +97,84 @@ public class MyReceiver extends BroadcastReceiver
         Intent intent = new Intent(context, MyReceiver.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 1, intent, PendingIntent.FLAG_NO_CREATE);
         return pi != null;
+    }
+
+//    public static void startActivityNotification(Context context, int notificationID, ReminderItem item)
+//    {
+//        NotificationManager mNotificationManager =
+//                (NotificationManager)
+//                        context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        //Create GPSNotification builder
+//        NotificationCompat.Builder mBuilder;
+//
+//        //Initialise ContentIntent
+//        Intent ContentIntent = new Intent(context, ActivityItemView.class);
+//        ContentIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        PendingIntent ContentPendingIntent = PendingIntent.getActivity(context,
+//                0,
+//                ContentIntent,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        mBuilder = new NotificationCompat.Builder(context)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setContentTitle(item.getTitle())
+//                .setContentText(item.getDescription())
+//                .setColor(context.getResources().getColor(R.color.colorPrimaryDark))
+//                .setAutoCancel(true)
+//                .setContentIntent(ContentPendingIntent)
+//                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE)
+//                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID,
+//                    "Activity Opening Notification",
+//                    NotificationManager.IMPORTANCE_HIGH);
+//            mChannel.enableLights(true);
+//            mChannel.enableVibration(true);
+//            mChannel.setDescription("Activity opening notification");
+//
+//            mBuilder.setChannelId(CHANNEL_ID);
+//
+//            Objects.requireNonNull(mNotificationManager).createNotificationChannel(mChannel);
+//        }
+//
+//        Objects.requireNonNull(mNotificationManager).notify(TAG_NOTIFICATION,notificationID,
+//                mBuilder.build());
+//    }
+
+    public static void startNotification(Context context, int notificationID, ReminderItem item)
+    {
+        NotificationManager mNotificationManager =
+                (NotificationManager)
+                        context.getSystemService(Context.NOTIFICATION_SERVICE);
+        //Create GPSNotification builder
+        NotificationCompat.Builder mBuilder;
+
+        mBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(item.getTitle())
+                .setContentText(item.getDescription())
+                .setColor(context.getResources().getColor(R.color.colorPrimaryDark))
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID,
+                    "Activity Opening Notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+            mChannel.enableLights(true);
+            mChannel.enableVibration(true);
+            mChannel.setDescription("Activity opening notification");
+
+            mBuilder.setChannelId(CHANNEL_ID);
+
+            Objects.requireNonNull(mNotificationManager).createNotificationChannel(mChannel);
+        }
+
+        Objects.requireNonNull(mNotificationManager).notify(TAG_NOTIFICATION,notificationID,
+                mBuilder.build());
     }
 
 }
